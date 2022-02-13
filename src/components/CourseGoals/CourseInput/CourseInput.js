12516831d3d53bac019b59e24a3088bae9ba380c
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-13 11:56:09 +0530

    Add empty input flag

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isEmptyInput, setIsEmptyInput] = useState(false);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+    setIsEmptyInput(false);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+
+    if (enteredValue.trim().length === 0) {
+      setIsEmptyInput(true);
+      setTimeout(() => {
+        setIsEmptyInput(false);
+      }, 700);
+    } else props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          placeholder={isEmptyInput ? "Empty input!!" : ""}
+          style={isEmptyInput ? { backgroundColor: "#EE6666" } : {}}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
